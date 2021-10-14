ce7f9bc1d60649a49ee74e6db765fa7939dfe402
DevExpressExampleBot <fakeEmail>
2021-10-14 11:35:50 +0300

    autogenerate VB

--- VB/WebDashboardAspNetCore3/Controllers/HomeController.vb
+++ VB/WebDashboardAspNetCore3/Controllers/HomeController.vb
@@ -1,33 +1,30 @@
-﻿Imports System
-Imports System.Collections.Generic
 Imports System.Diagnostics
-Imports System.Linq
-Imports System.Threading.Tasks
 Imports Microsoft.AspNetCore.Mvc
 Imports Microsoft.Extensions.Logging
 Imports WebDashboardAspNetCore3.Models
 
 Namespace WebDashboardAspNetCore3.Controllers
-	Public Class HomeController
-		Inherits Controller
 
-		Private ReadOnly _logger As ILogger(Of HomeController)
+    Public Class HomeController
+        Inherits Controller
 
-		Public Sub New(ByVal logger As ILogger(Of HomeController))
-			_logger = logger
-		End Sub
+        Private ReadOnly _logger As ILogger(Of HomeController)
 
-		Public Function Index() As IActionResult
-			Return View()
-		End Function
+        Public Sub New(ByVal logger As ILogger(Of HomeController))
+            _logger = logger
+        End Sub
 
-		Public Function Privacy() As IActionResult
-			Return View()
-		End Function
+        Public Function Index() As IActionResult
+            Return MyBase.View()
+        End Function
 
-		<ResponseCache(Duration := 0, Location := ResponseCacheLocation.None, NoStore := True)>
-		Public Function [Error]() As IActionResult
-			Return View(New ErrorViewModel With {.RequestId = If(Activity.Current?.Id, HttpContext.TraceIdentifier)})
-		End Function
-	End Class
+        Public Function Privacy() As IActionResult
+            Return MyBase.View()
+        End Function
+
+        <ResponseCache(Duration:=0, Location:=ResponseCacheLocation.None, NoStore:=True)>
+        Public Function [Error]() As IActionResult
+            Return MyBase.View(New ErrorViewModel With {.RequestId = If(Activity.Current?.Id, HttpContext.TraceIdentifier)})
+        End Function
+    End Class
 End Namespace
