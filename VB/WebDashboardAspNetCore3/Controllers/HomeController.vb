372ba05b5a3eaffdd9d1492878a6b6bb3bdddfb4
DevExpressExampleBot <fakeEmail>
2022-02-17 00:11:51 +0300

    autogenerate VB

--- VB/WebDashboardAspNetCore3/Controllers/HomeController.vb
+++ VB/WebDashboardAspNetCore3/Controllers/HomeController.vb
@@ -15,16 +15,16 @@ Namespace WebDashboardAspNetCore3.Controllers
         End Sub
 
         Public Function Index() As IActionResult
-            Return MyBase.View()
+            Return View()
         End Function
 
         Public Function Privacy() As IActionResult
-            Return MyBase.View()
+            Return View()
         End Function
 
         <ResponseCache(Duration:=0, Location:=ResponseCacheLocation.None, NoStore:=True)>
         Public Function [Error]() As IActionResult
-            Return MyBase.View(New ErrorViewModel With {.RequestId = If(Activity.Current?.Id, HttpContext.TraceIdentifier)})
+            Return View(New ErrorViewModel With {.RequestId = If(Activity.Current?.Id, HttpContext.TraceIdentifier)})
         End Function
     End Class
 End Namespace
